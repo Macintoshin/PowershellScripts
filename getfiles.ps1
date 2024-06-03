f0383ab55c2f0723aab69cc5f7a6446216713cca
Easfrq <80288917+Macintoshin@users.noreply.github.com>
2024-06-02 22:50:07 -0400

    Add files via upload

---
+++ getfiles.ps1
@@ -0,0 +1,2 @@
+Invoke-WebRequest -URI "https://download-installer.cdn.mozilla.net/pub/firefox/releases/126.0.1/win32/en-US/Firefox%20Installer.exe" -OutFile "C:\Users\$env:username\Downloads"
+pause
